72628cbe90be63076457b9c061b22dbbc56f0715
midnight077 <nishitj077@gmail.com>
2025-12-22 15:50:42 +0530

    measuring time without parallel computing

--- main.cpp
+++ main.cpp
@@ -51,50 +51,23 @@ int main(){
 
     KDTree tree(k);
     tree.build(points);
-        
-    // auto startBuildTime = chrono::high_resolution_clock::now();
-    // auto stopBuildTime = chrono::high_resolution_clock::now();
-    // auto durationBuildTime = chrono::duration_cast<chrono::milliseconds>(stopBuildTime - startBuildTime);
-    // std::cout<<durationBuildTime.count();
-    // std::cout << "\nLSH built successfully!" << endl;
 
-    // string build_file_name = "lsh_build_Lkw"+ to_string(L) + to_string(numHashes) + to_string(binWidth) + ".csv";
-    // writeDataToCSV(n,k,L,numHashes,durationBuildTime.count(), build_file_name);
-            
-            
-    // Print bucket information
-    // lsh.printAllBuckets();
-    // Step 4: Input query point
+    auto startTime = chrono::high_resolution_clock::now();
+    
     for( int i = 0 ; i < nqp ; i++){
         auto result = lsh.findNearestNeighbor(query_point[i]);
         Point nearest_lsh = lsh.getPoint(result.first);
-        nearest_lsh.print();
+        // nearest_lsh.print();
         Point nearest_kd = tree.findNearestNeighbor(query_point[i]);
-        nearest_kd.print();
+        // nearest_kd.print();
         int difference = nearest_lsh.distance(nearest_kd);
-
+        
         writeComparisonToCSV("./difference.csv", query_point[i], nearest_lsh, nearest_kd, difference, i == 0);
     }
     
-            // if (result.first == -1) {
-            //     std::cout << "Error: Could not find nearest neighbor!" << endl;
-            //     continue;
-            // }
-            
-            // cout << "\nNearest Neighbor: Point " << result.first << " -> ";
-            // lsh.getPoint(result.first).print();
-            // cout << endl;
-            
-            // // Step 6: Print distance
-            // cout << "\n--- Distance Calculation ---" << endl;
-            // cout << "Distance between query point and nearest neighbor: " 
-            //     << fixed << setprecision(4) << result.second << endl;
-            
-            // // Verification with linear search
-            // cout << "\n--- Verification (Linear Search) ---" << endl;
-            // auto trueResult = lsh.linearSearchNearest(queryPoint);
-            // cout << "True Nearest Neighbor: Point " << trueResult.first << " -> ";
-            // lsh.getPoint(trueResult.first).print();
-            std::cout << endl;
+    auto stopTime = chrono::high_resolution_clock::now();
+    auto durationBuildTime = chrono::duration_cast<chrono::milliseconds>(stopTime - startTime);
+    cout<<endl<<durationBuildTime.count()<<endl;
+    std::cout << endl;
     return 0;
 }
